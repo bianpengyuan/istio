f726b626764b0c2000592c65ba9f7db141ac0256
Pengyuan Bian <bianpengyuan@google.com>
2018-10-15 15:28:02 -0700

    something that all works.

--- mixer/tools/mixgen/cmd/server.go
+++ mixer/tools/mixgen/cmd/server.go
@@ -22,6 +22,7 @@ func serverGenCmd(rawArgs []string, printf, fatalf shared.FormatFn) *cobra.Comma
 	var packages []string
 	var templateFiles []string
 	var adapterPackage string
+	var configPackage string
 	serverCmd := &cobra.Command{
 		Use:   "server",
 		Short: "creates server code from the given templates",
@@ -33,7 +34,14 @@ func serverGenCmd(rawArgs []string, printf, fatalf shared.FormatFn) *cobra.Comma
 				fatalf("Invalid path %s: %v", outServerFile, err)
 			}
 
-			generator := ServerGenerator{OutServerPath: outServerFile, AdapterName: adapterName, AdapterPackage: adapterPackage, TemplatesPath: templateFiles, Packages: packages}
+			generator := ServerGenerator{
+				OutServerDir:   outServerFile,
+				AdapterName:    adapterName,
+				AdapterPackage: adapterPackage,
+				TemplatesPath:  templateFiles,
+				Packages:       packages,
+				ConfigPackage:  configPackage,
+			}
 			if err := generator.Generate(); err != nil {
 				fatalf("%v", err)
 			}
@@ -43,6 +51,7 @@ func serverGenCmd(rawArgs []string, printf, fatalf shared.FormatFn) *cobra.Comma
 		"supported template names")
 	serverCmd.PersistentFlags().StringVar(&outServerFile, "go_out", "./generated.go", "Output "+
 		"file path for generated template based go types and interfaces.")
+	serverCmd.PersistentFlags().StringVar(&configPackage, "config_package", "", "")
 	serverCmd.PersistentFlags().StringArrayVarP(&packages, "packages", "p", []string{},
 		"")
 	serverCmd.PersistentFlags().StringVar(&adapterPackage, "adapter_package", "", "")
@@ -52,11 +61,13 @@ func serverGenCmd(rawArgs []string, printf, fatalf shared.FormatFn) *cobra.Comma
 
 // ServerGenerator ...
 type ServerGenerator struct {
-	OutServerPath  string
-	AdapterName    string
-	AdapterPackage string
-	TemplatesPath  []string
-	Packages       []string
+	OutServerDir      string
+	AdapterName       string
+	AdapterPackage    string
+	AdapterConfigPath string
+	TemplatesPath     []string
+	Packages          []string
+	ConfigPackage     string
 }
 
 // Generate ...
@@ -76,7 +87,15 @@ func (sg *ServerGenerator) Generate() error {
 			return err
 		}
 	}
-	f1, err := os.Create(sg.OutServerPath)
+	if _, err := os.Stat(sg.OutServerDir + "/nosession"); os.IsNotExist(err) {
+		err = os.MkdirAll(sg.OutServerDir+"/nosession", os.ModePerm)
+		if err != nil {
+			return err
+		}
+	} else if err != nil {
+		return err
+	}
+	f1, err := os.Create(sg.OutServerDir + "/nosession/nosession.go")
 	if err != nil {
 		return err
 	}
@@ -86,7 +105,38 @@ func (sg *ServerGenerator) Generate() error {
 		_ = os.Remove(f1.Name())
 		return err
 	}
+
+	f2, err := os.Create(sg.OutServerDir + "/main.go")
+	if err != nil {
+		return err
+	}
+
+	mainFileData, err := sg.getMainGoContent(sg.AdapterName, sg.OutServerDir, oopMainTempl)
+	if _, err = f2.Write(mainFileData); err != nil {
+		_ = f2.Close()
+		_ = os.Remove(f2.Name())
+		return err
+	}
 	return nil
+}
+
+func (sg *ServerGenerator) getMainGoContent(adapterName, outServerPath, oopMainTempl string) ([]byte, error) {
+	type cmdMain struct {
+		AdapterName string
+		PackagePath string
+	}
+	pp := strings.TrimPrefix(outServerPath, os.Getenv("GOPATH")+"/src/")
+	cm := cmdMain{AdapterName: adapterName, PackagePath: pp}
+	mainTmpl, err := template.New("ProcMain").Funcs(
+		template.FuncMap{
+			"Capitalize": strings.Title,
+		}).Parse(oopMainTempl)
+	mainBuf := new(bytes.Buffer)
+	err = mainTmpl.Execute(mainBuf, cm)
+	if err != nil {
+		return nil, fmt.Errorf("cannot execute the template with the given data: %v", err)
+	}
+	return mainBuf.Bytes(), nil
 }
 
 func (sg *ServerGenerator) getServerGoContent(models []*modelgen.Model, adapterName, adapterPackage, noSessionTmpl string, packages []string) ([]byte, error) {
@@ -94,9 +144,11 @@ func (sg *ServerGenerator) getServerGoContent(models []*modelgen.Model, adapterN
 		AdapterName    string
 		AdapterPackage string
 		Packages       []string
+		ConfigPath     string
 		Models         []*modelgen.Model
 	}
-	ms := MS{AdapterName: adapterName, AdapterPackage: adapterPackage, Models: models, Packages: packages}
+	ms := MS{AdapterName: adapterName, AdapterPackage: adapterPackage, Models: models, Packages: packages, ConfigPath: sg.ConfigPackage}
+	importProto := false
 	serverTmpl, err := template.New("ProcServer").Funcs(
 		template.FuncMap{
 			"Capitalize": strings.Title,
@@ -132,6 +184,10 @@ func (sg *ServerGenerator) getServerGoContent(models []*modelgen.Model, adapterN
 				}
 				return modelgen.MessageInfo{}
 			},
+			"AddProtoToImpt": func() string {
+				importProto = true
+				return ""
+			},
 		}).Parse(noSessionTmpl)
 	if err != nil {
 		return nil, fmt.Errorf("cannot load template: %v", err)
@@ -142,5 +198,11 @@ func (sg *ServerGenerator) getServerGoContent(models []*modelgen.Model, adapterN
 	if err != nil {
 		return nil, fmt.Errorf("cannot execute the template with the given data: %v", err)
 	}
-	return serverBuf.Bytes(), nil
+	var retBytes []byte
+	if importProto {
+		retBytes = bytes.Replace(serverBuf.Bytes(), []byte("$$additional_imports$$"), []byte("proto \"github.com/gogo/protobuf/types\""), 1)
+	} else {
+		retBytes = bytes.Replace(serverBuf.Bytes(), []byte("$$additional_imports$$"), []byte(""), 1)
+	}
+	return retBytes, nil
 }
