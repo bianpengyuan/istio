d60d452913cc0201695e3ac645aa69b0df57ecda
Pengyuan Bian <bianpengyuan@google.com>
2020-06-15 18:30:40 +0000

    control plane metrics

---
+++ pilot/pkg/serviceregistry/kube/controller/gcpmonitoring_test.go
@@ -0,0 +1,55 @@
+// Copyright Istio Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package controller
+
+import (
+	"errors"
+	"fmt"
+	"testing"
+	"time"
+
+	"go.opencensus.io/stats/view"
+
+	gm "istio.io/istio/pilot/pkg/gcpmonitoring"
+	"istio.io/istio/pkg/test/util/retry"
+)
+
+func TestGCPMonitoringPilotK8sRegEvents(t *testing.T) {
+	exp := &gm.TestExporter{Rows: make(map[string][]*view.Row)}
+	view.RegisterExporter(exp)
+	view.SetReportingPeriod(1 * time.Millisecond)
+
+	incrementEvent("foo", "bar")
+	if err := retry.UntilSuccess(func() error {
+		exp.Lock()
+		defer exp.Unlock()
+		got := float64(0)
+		if len(exp.Rows["config_event_count"]) < 1 {
+			return errors.New("wanted metrics not received")
+		}
+		r := exp.Rows["config_event_count"][0]
+		if gm.FindTagWithValue("operation", "bar", r.Tags) && gm.FindTagWithValue("type", "foo", r.Tags) {
+			if sd, ok := r.Data.(*view.SumData); ok {
+				got = sd.Value
+			}
+		}
+		if got != 1.0 {
+			return fmt.Errorf("bad value for config_event_count: %f, want 1.0", got)
+		}
+		return nil
+	}); err != nil {
+		t.Fatalf("failed to get expected metric: %v", err)
+	}
+}
